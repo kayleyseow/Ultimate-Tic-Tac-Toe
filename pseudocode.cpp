9d7f5e054fb715868ffbbc13488e3e84718ee0ef
randomtanki <zhivincent@163.com>
2020-11-24 12:50:54 -0800

    mostly doneish framework

--- pseudocode.cpp
+++ pseudocode.cpp
@@ -14,12 +14,35 @@ void tutorial ()
 }
 
 
+winCheck (int board[9])
+{
+    //for large board
+    //returns 0 if nobody won
+    //returns 1 if X wins
+    //returns 2 if O wins
+}
+
+
+int winCheck(int x)
+{
+    //for small board
+    //returns 0, 1, or 2
+    //decode intoX&O
+    //put into other wincheck
+}
+
+
 int getPos(int oldb[9], int board[9])
 {
-    int temp[9]
+    //returns 0-9 depending on the position- 0 if anywhere is okay, 1-9 for box positions.
+    //finds the point that was changed last time
+    int temp[9];
     temp[0] = board[0]-oldb[0];
     ...
     if (temp == [0,0,0,0,0,0,0,0,0]);
+    //do we need a no change?
+    //find the position to go to next
+    //check if that's taken using winCheck
     {
 
     }
@@ -28,14 +51,6 @@ int getPos(int oldb[9], int board[9])
 void draw (int board[9])
 {
     //shit.
-}
-
-
-wincheck (int board[9])
-{
-    //returns 0 if nobody won
-    //returns 1 if X wins
-    //returns 2 if O wins
 }
 
 
@@ -62,14 +77,14 @@ int main ()
         draw(board);
         while(winCheck(board)==0)
         {
-            int p = getPos(oldboard,board);
-            oldboard=board;
+            int p = getPos(oldb,board);
+            oldb=board;
             board=inputX();
             draw(board);
             if(winCheck(board)==0)
             {
-            getPos(oldboard, board);
-            oldboard=board;
+            p = getPos(oldb, board);
+            oldb=board;
             board=inputO();
             draw(board);
             }
