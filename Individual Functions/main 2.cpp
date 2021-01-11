bbf3fec0bae32cde97a8011b06fed00d88b12917
ellenfang77 <55215604+ellenfang77@users.noreply.github.com>
2021-01-10 19:46:53 -0800

    Add files via upload

---
+++ Individual Functions/main 2.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+using namespace std;
+
+string getPosition(){
+
+//determine mini boards A,B,C,D,E,F,G,H,I
+  int pos;
+
+  int board_num = pos%9;
+
+  char board;
+
+  switch(board_num){
+    case 1:
+      board = 'A';
+    case 2:
+      board = 'B';
+    case 3:
+      board = 'C';
+    case 4:
+      board = 'D';
+    case 5: 
+      board = 'E';
+    case 6:
+      board = 'F';
+    case 7:
+      board = 'G';
+    case 8:
+      board = 'H';
+    case 9:
+      board = 'I';
+  }
+
+  //if board is full, return -1; use function winCheck()
+  cout<<"enter a position";
+
+  cin<<pos;
+  
+  if(winCheck(board_num)== "X" || "O"){
+    return "Board already occupied. Try again.";
+  }
+  else{
+    return board + pos;
+  }
+  }
+}
